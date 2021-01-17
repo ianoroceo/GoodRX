79a02664a9fdde47ab5f528435146e47938f29ed
IAN OROCEO <ian.oroceo@ascendum.com>
2021-01-16 19:52:16 -0500

    adding eslint and ts configuration

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    '$': true,
+    '$$': true,
+    'browser': true,
+    'expect': true,
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+  },
+};
